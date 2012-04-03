72fafc9f61dc5161e2b3b529006ee011983abdb5
Alexey Gaziev <alex.gaziev@gmail.com>
2012-04-04 00:25:09 +0400

    Build is not okay. Travis don't get my failed spec

--- spec/tonic/fail_spec.rb
+++ spec/tonic/fail_spec.rb
@@ -2,7 +2,8 @@ require 'tonic'
 
 describe Tonic, '#not_working' do
   it 'really not working yet' do
-    raise 'Soon it will work'
     Tonic::VERSION.should == '0.0.2'
   end
 end
+
+raise 'Build is not ok'

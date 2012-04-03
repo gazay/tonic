e89e5b12fe64970e81558802f7e94c4cdbf0341b
Alexey Gaziev <alex.gaziev@gmail.com>
2012-04-03 22:05:46 +0400

    Added right path for template and force push command

--- lib/tonic.rb
+++ lib/tonic.rb
@@ -11,13 +11,27 @@ module Tonic
       create_branch
       remove_all_files
       copy_template
+      commit_changes
+    end
+
+    def push_pages
+      if in_gh_pages_branch?
+        sh 'git push -f origin gh-pages'
+      else
+        puts 'Forget about it bro'
+      end
     end
 
     private
 
+    def in_gh_pages_branch?
+      branches = sh('git branch')
+      branches.lines.any? { |it| it =~ /\* gh-pages/ }
+    end
+
     def branch_exists?
-      branches = sh 'git branch'
-      branches.any? { |it| it =~ /gh-pages/ }
+      branches = sh('git branch')
+      branches.lines.any? { |it| it =~ /gh-pages/ }
     end
 
     # TODO: Later I will think about how to do it through github-gem properly
@@ -30,10 +44,14 @@ module Tonic
       sh 'rm -rf ./*'
     end
 
+    def commit_changes
+      sh "git commit -am 'Init github pages'"
+    end
+
     def copy_template
-      path =  File.expand_path('../template', __FILE__)
+      path =  File.expand_path('../../template', __FILE__)
       puts path
-      sh "cp #{path}/* ./"
+      sh "cp -r #{path}/* ./"
     end
 
     def sh(*command)
